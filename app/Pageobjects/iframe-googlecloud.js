af14a0c5cf47c70209a98548e4c7205e8e711009
Sherzod Naziraliev <109876816+sherzodnaziraliev5027@users.noreply.github.com>
2023-01-21 16:44:31 +0500

    Delete app/Pageobjects directory

--- app/Pageobjects/iframe-googlecloud.js
+++
@@ -1,9 +0,0 @@
-class Iframe{
-    get checkExisting(){
-        return $("td[style*='logo']")
-    }
-    get getestimatedCost(){
-        return $("//h3[contains(text(),'USD')]").getText()
-    }
-}
-module.exports=new Iframe
